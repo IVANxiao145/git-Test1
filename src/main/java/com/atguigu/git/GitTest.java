fddfcc035d9d12ab4b49a31f92726e05689706a0
IVAN <1452478201@qq.com>
2022-11-29 15:07:24 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @Auther:IVAN
+ * @Time:2022-11-29-15:01
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("你好，git！");
+    }
+}
